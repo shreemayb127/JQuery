ac5143526831f924dd6915cd417b4c35b1656412
Shreemay Tripathi <4108shreemay@gmail.com>
2024-08-02 09:28:09 +0530

    JQuerydata

---
+++ practice/1/index1.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $("#hide").click(function(){
+    $("p").hide();
+});
+
+    $("#show").click(function(){
+    $("p").show();
+    })
+
+    $("#tog").click(function(){
+    $("p").toggle();
+    })
+
+
+})
---
+++ task/Galary/2.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function () {
+
+    $(".btn").click(function () {
+        $(".gal1").css({
+            top: "0",
+            left: "0",
+        });
+
+        $(".gal2").css({
+            top: "160px",
+            left: "160px",
+        });
+
+        $(".gal3").css({
+            top: "0",
+            left: "320px",
+        });
+        
+        $(".gal4").css({
+            top: "160px",
+            left: "480px",
+        });
+
+        $(".gal5").css({
+            top: "0",
+            left: "640px",
+        });
+
+    });
+
+})
